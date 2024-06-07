d18de089db3feab24b219eae65dc509b69122d8d
spongxin <2624490979@qq.com>
2024-06-07 21:51:52 +0800

    update assign_tasks

--- process_grobid.py
+++ process_grobid.py
@@ -34,17 +34,10 @@ def processFulltextDocument(file_path, save_path, port):
 def assign_tasks(file_dir, save_dir, ports=[8086, 8186, 8286, 8386], process_func=processHeaderDocument):
     file_paths = [os.path.join(file_dir, filename) for filename in os.listdir(file_dir) if filename.endswith('.pdf')]
 
-    port_file_tasks = {}
-    for port in ports:
-        port_file_tasks[port] = []
-    for i, file_path in enumerate(file_paths):
-        port = ports[i % len(ports)]
-        port_file_tasks[port].append((file_path, os.path.join(save_dir, os.path.basename(file_path)), port))
-    
     with Pool(processes=len(ports)) as pool:
-        for port, tasks in port_file_tasks.items():
-            for task in tqdm(tasks, total=len(tasks), desc=f'Processing files on port {port}'):
-                pool.starmap(process_func, task)
+        for i, file_path in tqdm(enumerate(file_paths), total=len(file_paths), desc='Assigning tasks'):
+            port = ports[i % len(ports)]
+            pool.starmap(process_func, (file_path, os.path.join(save_dir, os.path.basename(file_path).replace('.pdf', '.xml')), port))
     logging.info('All tasks finished.')
 
 if __name__ == '__main__':
